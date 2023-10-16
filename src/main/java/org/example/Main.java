89b6d65f07a8a7efe5eca2a5f424920b5a26bb3b
Theo Minarini <=>
2023-10-16 20:33:52 -0300

    Criada a integração com neo4j

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,23 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.*;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.server.RoutingHandler;
-
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import org.neo4j.driver.*;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        RoutingHandler routingHandlerPeople = new RoutingHandler().get("/people", new HttpHandlerManagerPeoplesRegister());
+        RoutingHandler routingHandlerPeople = new RoutingHandler()
+                .get("/people", new HttpHandlerManagerPeoplesRegister());
         Undertow server = Undertow.builder()
                 .addHttpListener(8080, "localhost")
                 .setHandler(routingHandlerPeople)
@@ -21,22 +25,29 @@ public class Main {
         server.start();
     }
 
-    public String[] getJson(String json) {
-        return null;
+    public List<Object> getJson(String json) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode node = objectMapper.readTree(json);
+        List<Object> nodes = new ArrayList<>();
+        nodes.add(node.get("name").toString());
+        nodes.add(node.get("apelido").toString());
+        nodes.add(node.get("nascimento").toString());
+        if (node.get("stack").isArray()) {
+            // Converte o JsonNode em um array de inteiros
+            nodes.add(objectMapper.treeToValue(node.get("stack"), String[].class));
+        }
+        return nodes;
     }
 
-    public static java.sql.Connection driverManager() throws SQLException {
-        return DriverManager.getConnection(
-                "postgresql:http://localhost:5432/Rinha",
-                "root",
-                "root"
-        );
-
+    public static Driver driverManager() throws SQLException {
+        return GraphDatabase.driver("localhost:7673",
+                AuthTokens.basic("neo4j", "neo4j"));
     }
 
-    public static class HttpHandlerManagerPeoplesRegister implements HttpHandler {
+    public static class run1 implements Runnable {
+
         @Override
-        public void handleRequest(HttpServerExchange exchange) throws Exception {
+        public void run() {
             ConnectionFactory connectionFactory = new ConnectionFactory();
             connectionFactory.setHost("localhost");
             connectionFactory.setPort(5672);
@@ -57,26 +68,39 @@ public class Main {
                 System.out.println("------erro------");
             }
         }
+    }
 
-        public static DeliverCallback defaultConsumer() {
-            return (consumerTag, message) -> {
-                String body = Arrays.toString(message.getBody());
-                try {
-                    java.sql.Connection connection = driverManager();
-                    connection.beginRequest();
-                    PreparedStatement stm = connection.prepareStatement(
-                            """ 
-                                    CREATE TABLE (
-                                           id SERIAL PRIMARY_KEY
-                                    );
-                                    """.trim()
-                    );
-                    connection.commit();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("hehe");
-            };
+    public static class HttpHandlerManagerPeoplesRegister implements HttpHandler {
+        @Override
+        public void handleRequest(HttpServerExchange exchange) throws Exception {
+            new run1().run();
         }
+
+
+    }
+
+    public static class runPersist implements Runnable {
+
+        @Override
+        public void run() {
+            Driver driver = null;
+            try {
+                driver = driverManager();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            Session session = driver.session();
+            session.beginTransaction();
+            session.run("CREATE (n{name:''})");
+            session.close();
+        }
+    }
+
+    public static DeliverCallback defaultConsumer() {
+        return (consumerTag, message) -> {
+            String body = Arrays.toString(message.getBody());
+            new runPersist().run();
+            System.out.println("hehe");
+        };
     }
 }
